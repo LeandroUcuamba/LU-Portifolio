d88b2c966306198387ff03f5388149ea2861d5ce
Leandro Ucuamba <104640070+LeandroUcuamba@users.noreply.github.com>
2022-12-28 13:25:12 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initMenuMobile from "./modules/menu-mobile.js";
+import initMenuTransition from "./modules/menu-transition.js";
+import initTabNav from "./modules/tabnav.js";
+
+
+
+initTabNav();
+initMenuTransition();
+initMenuMobile();
